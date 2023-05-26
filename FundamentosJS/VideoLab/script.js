2f027e926da788901f1b51ef1450b5a5207ac98a
cpinot <cynthia.priscila.pino@gmail.com>
2023-05-25 21:19:29 -0400

    VideoLab

---
+++ FundamentosJS/VideoLab/script.js
@@ -0,0 +1,8 @@
+function play(element)  {
+    var video = document.getElementById("video");
+    video.play();
+}
+function pause(element)  {
+    var video = document.getElementById("video");
+    video.pause();
+}
