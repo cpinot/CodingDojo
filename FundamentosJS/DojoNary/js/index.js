d9a9f0ed705447b8fe5421f6032b6aabb021fe48
cpinot <cynthia.priscila.pino@gmail.com>
2023-05-25 20:02:53 -0400

    Dojonary

--- FundamentosJS/DojoNary/js/index.js
+++ FundamentosJS/DojoNary/js/index.js
@@ -1,8 +1,15 @@
 function agregarLike(elemento)  {
     var likes = parseInt(elemento.innerText);
     likes++;
-    elemento.innerText = likes;
+    elemento.innerText = likes +" Likes";
+    alert("Ninja was liked");
 }
 function agregarHtml(elemento) {
     let contenedor = document.querySelector('".flex-2');
+}
+function eliminar(elemento) {
+    elemento.remove();
+}
+function login(elemento) {
+    elemento.innerText = "Logout";
 }
