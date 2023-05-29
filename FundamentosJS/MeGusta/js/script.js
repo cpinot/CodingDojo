08a64c02de284f0b311e952a7785996f2a0a1583
cpinot <cynthia.priscila.pino@gmail.com>
2023-05-29 00:41:55 -0400

    Me Gusta

--- FundamentosJS/MeGusta/js/script.js
+++ FundamentosJS/MeGusta/js/script.js
@@ -1,7 +1,12 @@
-function likes() {
-    var likes = document.querySelector(".likeAmigo");
-    console.log(likes);
-    var likesNumber = parseInt(likes.innerHTML);
-    likesNumber++;
-    likes.innerHTML = likesNumber + " Like(s)";
+var likesNumber =[9, 12, 9];
+var boton = [
+    document.querySelector("#btn-0"),
+    document.querySelector("#btn-1"),
+    document.querySelector("#btn-2")
+];
+
+function like(id) {
+    console.log(id);
+    likesNumber[id]++;
+    boton[id].innerHTML = likesNumber[id] + " like(s)";
 }
