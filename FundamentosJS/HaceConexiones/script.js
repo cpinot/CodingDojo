03781ee456c73ac19d1d294f5ba3fa7dd94b0506
cpinot <cynthia.priscila.pino@gmail.com>
2023-06-05 14:31:10 -0400

    Conexiones

--- FundamentosJS/HaceConexiones/script.js
+++ FundamentosJS/HaceConexiones/script.js
@@ -6,43 +6,26 @@ var amigos = document.querySelector("#amigos");
 var solicitudes = document.querySelector("#solicitudes");
 
 //Remover elementos en botones de +
-for (index = 0; index < addBtns.length; index++) {
-    console.log(addBtns[index]);
-    let element = addBtns[index];
-    element.addEventListener("click", () => {
-        let elementoRemover = element.parentElement.parentElement;
-        elementoRemover.remove();
-        let numSolicitudes = parseInt(solicitudes.innerHTML);
-        let numAmigos = parseInt(amigos.innerHTML);
-        solicitudes.innerHTML = numSolicitudes - 1;
-        amigos.innerHTML = numAmigos + 1;
-    })
+function aceptarSolicitud(element){
+    let elementoRemover = element.parentElement.parentElement;
+    elementoRemover.remove();
+    let numSolicitudes = parseInt(solicitudes.innerHTML);
+    let numAmigos = parseInt(amigos.innerHTML);
+    solicitudes.innerHTML = numSolicitudes - 1;
+    amigos.innerHTML = numAmigos + 1;
+    
 }
-
-//Remover elementos en botones de -
-for (index = 0; index < removeBtns.length; index++) {
-    console.log(removeBtns[index]);
-    let element = removeBtns[index];
-    element.addEventListener("click", () => {
-        let elementoRemover = element.parentElement.parentElement;
-        elementoRemover.remove();
-        let numSolicitudes = parseInt(solicitudes.innerHTML);
-        solicitudes.innerHTML = numSolicitudes - 1;
-    })
-}
+function removeElement(element){
+    let elementoRemover = element.parentElement.parentElement;
+    elementoRemover.remove();
+    let numSolicitudes = parseInt(solicitudes.innerHTML);
+    let numAmigos = parseInt(amigos.innerHTML);
+    solicitudes.innerHTML = numSolicitudes - 1;
+    }
 
 
-function cambiarNombre() {
-    let elementoNombre = document.getElementById("nombre");
-    console.log(elementoNombre.innerHTML);
-    elementoNombre.innerHTML = "Ing. Nestor Ribero ";
-}
-
-
-function editProfile(element){
+function editProfile(){
     let cardName=document.querySelector(".cardName");
-    cardName.innerText="Cynthia Pino"
-}
-function removerUser(element){
-
+    console.log(cardName);
+    cardName.innerText="Cynthia Pino";
 }
