4571ef1924830ecefef8bf89870be8df0a35b509
cpinot <cynthia.priscila.pino@gmail.com>
2023-06-08 10:21:50 -0400

    hola mundo!

--- python/fundamentals/fundamentals/hola_mundo.py
+++ python/fundamentals/fundamentals/hola_mundo.py
@@ -6,8 +6,9 @@ print( "Hola,", name)	# con una coma
 print( "Hola,"+ name )	# con un +
 # 3. imprimir "Hola 42!" con el número en una variable
 name = 42
-print( "Hola!", name)	# con una coma
-print( "Hola,",+name )	# con una +	-- este debería arrojar un error!
+print( "Hola!,", name)	# con una coma
+print( "Hola!,",+name )# con una +	-- este debería arrojar un error!
+print(f"hola!, {name}")	# con formato f
 # 4. imprimir "Amo comer sushi y pizza" con las comidas en variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
